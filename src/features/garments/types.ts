f6846ee56ff959e157b8ada6f4f12d50fa7bd99f
Augusto Gomez <agomezsantos95@gmail.com>
2024-07-06 13:20:00 +0200

    Add Garment model to project

--- src/features/garments/types.ts
+++ src/features/garments/types.ts
@@ -2,15 +2,18 @@ export interface GarmentStructure extends GarmentStructureWithoutId {
   _id: string;
 }
 
+type GarmentStyle = "gym" | "casual" | "formal" | "party";
+type GarmentGender = "woman" | "men";
+type GarmentSize = "medium" | "large" | "small" | "x-large";
 export interface GarmentStructureWithoutId {
   name: string;
-  size: "medium" | "large" | "small" | "x-large";
+  size: GarmentSize;
   colors: string[];
   stock: number;
   newArrivals: boolean;
   topSelling: boolean;
   price: number;
-  gender: "woman" | "men";
-  style: "gym" | "casual" | "formal" | "party";
+  gender: GarmentGender;
+  style: GarmentStyle;
   imageUrl: string;
 }
